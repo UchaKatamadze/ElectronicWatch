101cf7fe74c424bd83e1a9884a98c076679f2245
UchaKatamadze <u.katamadze@gmail.com>
2023-05-18 15:21:30 +0400

    Solved

--- src/main/java/com/epam/rd/autotasks/meetautocode/ElectronicWatch.java
+++ src/main/java/com/epam/rd/autotasks/meetautocode/ElectronicWatch.java
@@ -4,8 +4,26 @@ import java.util.Scanner;
 
 public class ElectronicWatch {
     public static void main(String[] args) {
+        // asking input from the user
         Scanner scanner = new Scanner(System.in);
         int seconds = scanner.nextInt();
+
+        // defining seconds, minutes and hours per day
+        int second1 = 60*60*24;
+        int secondsInAnHour = 60 * 60;
+        int minutes = (seconds % secondsInAnHour) / 60;
+        int hours = (seconds % second1) / secondsInAnHour;
+        int second2 = seconds % 60;
+
+        // constructing clock
+        int secondsFirstDigit = second2 / 10;
+        int secondsSecondDigit = second2 % 10;
+        int minutesFirstDigit = minutes / 10;
+        int minutesSecondDigit = minutes % 10;
+
+
+        // displaying the final result
+        System.out.println(hours+ ":" + minutesFirstDigit + minutesSecondDigit + ":" + secondsFirstDigit + secondsSecondDigit);
 
 
     }
